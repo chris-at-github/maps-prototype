0cb2cfbc22b33ebed959c1912785ef1aef04343e
Christian Pschorr <pschorr.christian@gmail.com>
2014-11-03 23:18:39 +0100

    Start x und y bei 0

--- world/index.php
+++ world/index.php
@@ -1,8 +1,8 @@
 <?php include($_SERVER['DOCUMENT_ROOT'] . '/co3/header.php'); ?>
 
 <?php
-	// for($x = 1; $x <= 5; $x++) {
-	// 	for($y = 1; $y <= 3; $y++) {
+	// for($x = 0; $x <= 5; $x++) {
+	// 	for($y = 0; $y <= 3; $y++) {
 	// 		echo $x . 'x' . $y;
 	// 	}
 	// }
