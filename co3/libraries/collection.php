06d03ee92facd73fac4847988620d87e4117ec26
Christian Pschorr <pschorr.christian@gmail.com>
2014-11-10 18:20:24 +0100

    Init Collection

---
+++ co3/libraries/collection.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @version		$Id: collection.php
+ * @package		Co3
+ * @copyright	Copyright (C) 2014 Christian Pschorr
+ * @license		GNU/GPL
+ */
+class Co3Collection implements Iterator {
+
+	/**
+	 * wandelt das Object in ein JSON Objekt um
+	 *
+	 * @return string JSON String
+	 */
+	public function toJson() {
+
+	}
+}
+?>
